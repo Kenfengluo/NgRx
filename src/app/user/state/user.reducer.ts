d0cbf5560844b2ed1b3adc82bf3d71cbdf403f3e
Ken Luo <LUOK1@airproducts.com>
2018-10-23 16:05:45 -0400

    add strong type state interface

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,4 +1,6 @@
-export function reducer(state, action) {
+import { User } from '../user';
+
+export function reducer(state: UserState, action): UserState {
   switch (action.type) {
 
     case 'MASK_USER_NAME':
@@ -11,4 +13,9 @@ export function reducer(state, action) {
     default:
       return state;
   }
+}
+
+export interface UserState {
+  maskUserName: boolean;
+  currentUser: User;
 }
